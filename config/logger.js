0ec8606fd13a13f7532ffd9410e1bb04eed3445f
tonis.tammeorg <tonis.tammeorg@teleplan.com>
2018-07-26 23:38:55 +0300

    create logs dir

--- config/logger.js
+++ config/logger.js
@@ -1,11 +1,18 @@
-const logger = require('winston');
+const logger = require('winston'),
+    fs = require('fs');
+
+const LOG_DIR = './logs';
+
+if (!fs.existsSync(LOG_DIR)) {
+    fs.mkdirSync(LOG_DIR);
+}
 
 logger.clear();
 
 logger.cli();
 
 logger.add(logger.transports.File,
-{ filename: './logs/error.log', level: 'error' });
+    { filename: './logs/error.log', level: 'error' });
 
 if (process.env.NODE_ENV !== 'test') {
     logger.add(logger.transports.Console);
